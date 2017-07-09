c82d00c6ec945f6727a6aaf932a848fb44c30395
Tomoya Mikami <trymorecc@gmail.com>
2017-07-10 01:26:14 +0900

    Imageコントローラー生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'image/index'
+
+  get 'image/upload'
+
+  get 'image/save'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
