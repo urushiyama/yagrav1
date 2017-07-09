121fca48e3afa9497be5ea6282632ce97ee66647
Tomoya Mikami <trymorecc@gmail.com>
2017-07-10 01:22:46 +0900

    usersコントローラー生成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def profile
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/profile'
+
+  get 'users/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
