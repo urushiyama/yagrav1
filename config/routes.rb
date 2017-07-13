e1cd49143f8a666079bbaa4582dba8ae0f5a9c80
Tomoya Mikami <trymorecc@gmail.com>
2017-07-14 00:06:58 +0900

    profileコントローラー設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'profile/new'
+
+  get 'profile/create'
+
+  get 'profile/show'
+
+  get 'profile/edit'
+
+  get 'profile/update'
+
+  get 'profile/destroy'
+
   get 'sessions/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
