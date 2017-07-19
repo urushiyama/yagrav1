370c824f36d6201575fcb6b10ccb2ff3d897bc7d
Tomoya Mikami <trymorecc@gmail.com>
2017-07-20 00:23:51 +0900

    userコントローラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,15 +13,5 @@ Rails.application.routes.draw do
 
   get 'users/destroy'
 
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/profile'
-
-  get 'users/edit'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
