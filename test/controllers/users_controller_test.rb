0fca90a5957694e0a2278b8441ad2c2a5efa370a
urushiyama <aswif10flis1ntkb@gmail.com>
2017-07-20 03:27:41 +0900

    Issue 12に対する修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,24 +16,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get create" do
-    get users_create_url
-    assert_response :success
-  end
-
   test "should get edit" do
     get users_edit_url
     assert_response :success
   end
-
-  test "should get update" do
-    get users_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get users_destroy_url
-    assert_response :success
-  end
-
 end
